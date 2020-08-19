009babc195bf67c6c50bcf6eb9ddf6dfc583f4ca
Lela Smith <smith.lelaa@gmail.com>
2020-08-18 17:04:21 -0700

    tidy up code example

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,24 @@ $(document).ready(function() {
   $("form#fortune-quiz").submit(function(event){
     event.preventDefault();
 
-    // var score = [];
-
+    var score = [];
     $("input:checkbox[name=unlucky]:checked").each(function(){
-      var unlucky = parseInt($(this).val());
-      // score.push(unlucky);
-      $("input:checkbox[name=lucky]:checked").each(function(){
-        var lucky = parseInt($(this).val());
-        // score.push(lucky);
-
-        var totalScore = (unlucky + lucky)
-
-        if (totalScore > 2) {
-          $(".lucky").show();
-        } else if (totalScore < -2) {
-          $(".unlucky").show();
-        } else {
-          $(".neutral").show();
-        }
-        console.log(totalScore);
-
-      });
-
+      score.push(parseInt($(this).val()));
     });
+    $("input:checkbox[name=lucky]:checked").each(function(){
+      score.push(parseInt($(this).val()));
+    });
+      
+    var totalScore = score.reduce((a, b) => a + b, 0)
+    console.log(totalScore);
 
-
-
-
-
+      if (totalScore > 2) {
+        $(".lucky").show();
+      } else if (totalScore < -2) {
+        $(".unlucky").show();
+      } else {
+        $(".neutral").show();
+      }
 
   });
 });
